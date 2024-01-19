5a1b4c91b3cbd447ac6ab2cbbe7cbc5fafc9e542
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2024-01-19 09:07:05 -0500

    Update ram.py

--- backend/ram.py
+++ backend/ram.py
@@ -1,1 +1,1 @@
-ranch = ['18 naked cowboys in the showers at Ram Ranch!','Big, hard, throbbing cocks wanting to be sucked!','18 naked cowboys wanting to be fucked!','Cowboys in the showers at Ram Ranch!','On their knees wanting to suck cowboy cocks!','Ram Ranch really rocks!']
+ranch = ["output","I like ranch","I like buffalo wings", "I like blue cheese"]
